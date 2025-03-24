3cd09d919f0450cabc641c5c7b0ade5ff7857d94
Blaine Winslow <blaine.winslow@gmail.com>
2025-03-24 00:57:23 -0900

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -21,3 +21,14 @@ sudo usermod -aG docker $USER
 
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
+
+
+[NordVpn]
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+sudo usermod -aG nordvpn $USER
+
+[SyncThing]
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo systemctl enable syncthing@$USER.service
+sudo systemctl start syncthing@$USER.service
