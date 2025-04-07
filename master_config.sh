f3c7b5c38ad713b625093db097c1630964def003
Blaine Winslow <blaine.winslow@gmail.com>
2025-04-07 15:16:12 -0400

    Update master_config.sh

--- master_config.sh
+++ master_config.sh
@@ -121,7 +121,20 @@ sudo snap install terraform
 sudo snap install aws-cli --classic
 
 
+[Open Tofu] [If RedHat]
+# Download the installer script:
+curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
+# Alternatively: wget --secure-protocol=TLSv1_2 --https-only https://get.opentofu.org/install-opentofu.sh -O install-opentofu.sh
 
+# Give it execution permissions:
+chmod +x install-opentofu.sh
 
+# Please inspect the downloaded script
+
+# Run the installer:
+./install-opentofu.sh --install-method rpm
+
+# Remove the installer:
+rm -f install-opentofu.sh
 
 
